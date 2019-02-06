edb67c2f5ff2723d63a91dcec0aea108479374cd
Abhishek Gupta <abhishekgupta97023@gmail.com>
2019-02-06 23:19:26 +0530

    Toolbar added and full_screen issue resolved

--- app/src/main/java/com/example/bhavya/suyog/Activities/MainActivity.java
+++ app/src/main/java/com/example/bhavya/suyog/Activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.bhavya.suyog.Activities;
 
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.bhavya.suyog.HelperClass.Alarm;
@@ -15,16 +19,20 @@ import com.example.bhavya.suyog.HelperClass.AlarmAdapter;
 import com.example.bhavya.suyog.R;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private AlarmAdapter mAdapter;
     private TextView filter_zone;
+    private TextView toolbar_title;
     public static String search_type;
     private TextView filter_location;
     private Boolean location_selected=false;
     private Boolean Zone_selected=false;
     private RecyclerView.LayoutManager mLayoutManager;
+    private Toolbar Alarms_toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +44,25 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new AlarmAdapter(alarm_list);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
+
+
+        tool_bar();
+
+        if(Build.VERSION.SDK_INT>22)
+        {
+            //getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.background_grey));
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+           // getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
     }
 
     public ArrayList<Alarm> dataset(){
         ArrayList<Alarm> alarm_list_data=new ArrayList<>();
-        alarm_list_data.add(new Alarm("STL_TP_11","south","Trombay","Trombay-Parcel 1 T-2","S"));
+        alarm_list_data.add(new Alarm("STL_TP_11","South","Trombay","Trombay-Parcel 1 T-2","S"));
         alarm_list_data.add(new Alarm("STL_MH_103","North Central","Bhiwandi","Tawre Bldg","NC"));
         alarm_list_data.add(new Alarm("STL_MH_115","North West","Nalasopara","Ayan Apartment","NW"));
         alarm_list_data.add(new Alarm("STL_MH_110","North Central","Bhiwandi","Romiya Apartment","NC"));
@@ -128,5 +150,20 @@ public class MainActivity extends AppCompatActivity {
         else if(!location_selected && !Zone_selected) {
             search_type="Both";
         }
+
+
+    }
+    public void tool_bar()
+    {
+
+        Alarms_toolbar = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(Alarms_toolbar);
+        toolbar_title=findViewById(R.id.app_toolbar_name);
+      //  toolbar_title.setText("Alarms");
+        //toolbar_text.setText(R.string.ongoing_player);
+        //Objects.requireNonNull(getSupportActionBar()).setTitle("      Alarms     ");
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+
+
     }
 }
